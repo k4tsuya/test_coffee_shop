9b9565f0cabb2abc382f7d21ff8989018a3e43f3
liang <liang.huang@dci-student.org>
2024-08-30 15:07:06 +0200

    day 4 topic updates

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+// window.alert("This is a script!");
+
+const myName = document.getElementById("fname");
+console.log(myName)
+
+let allParagraphs = document.getElementsByTagName("p");
+console.log(allParagraphs)
+
+let allHeaders = document.getElementsByTagName("h1");
+console.log(allHeaders)
+
+let allClasses = document.getElementsByClassName("bio");
+console.log(allClasses)
